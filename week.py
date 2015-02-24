627cf83db1d80ca8cae791f01391cf0fcda78e23
Wei Yang <wei@weizilla.com>
2015-02-24 17:17:56 -0600

    add test html with bootstrap and jquery

--- week.py
+++ week.py
@@ -4,8 +4,11 @@ import argparse
 import re
 from bs4 import BeautifulSoup
 from itertools import tee, izip
+from collections import OrderedDict
 
 days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday", None]
+
+clean = ["<FONT FACE=\"Arial\">", "<FONT SIZE=-1>", "</FONT>", "<HR>", "</BODY>", "</HTML>" ]
 
 def pairwise(iterable):
     "s -> (s0,s1), (s1,s2), (s2, s3), ..."
@@ -14,22 +17,28 @@ def pairwise(iterable):
     return izip(a, b)
 
 class Week(object):
-    def __init__(self, htmlFile):
-        with open(htmlFile) as file:
-            self.soup = BeautifulSoup(file)
+    def __init__(self, filename):
+        with open(filename) as htmlFile:
+            self.html = htmlFile.read() 
+        self.clean()
         self.parse()
 
+    def clean(self):
+        for cleanText in clean:
+            self.html = self.html.replace(cleanText, "")
+
     def parse(self):
-        self.days = []
-        text = self.soup.get_text()
+        self.days = OrderedDict()
         for startDay, endDay in pairwise(days):
-            start = text.index(startDay)
+            startDayIndex = self.html.index(startDay)
+            start = self.html.index("<BR>", startDayIndex)
             if endDay:
-                end = text.index(endDay)
-                workout = text[start:end]
+                endDayIndex = self.html.index(endDay)
+                end = self.html.rindex("<DIV ALIGN=right>", startDayIndex, endDayIndex)
+                workout = self.html[start:end]
             else:
-                workout = text[start:]
-            self.days.append(Day(startDay, workout.strip()))
+                workout = self.html[start:]
+            self.days[startDay] = Day(startDay, workout.strip())
 
 class Day(object):
     def __init__(self, day, workout):
@@ -44,7 +53,7 @@ def parseArgs():
 if __name__ == "__main__":
     args = parseArgs()
     week = Week(args.filename)
-    for day in week.days:
+    for day in week.days.values():
         print day.day
         print day.workout
         print "====================================="
