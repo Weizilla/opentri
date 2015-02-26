f5d0ca31428acf50a8b69d3974a41b800c3ceb80
Wei Yang <wei@weizilla.com>
2015-02-26 15:31:47 -0600

    clean up css, better viewpoints for mobile

--- html/opentri.js
+++ html/opentri.js
@@ -1,19 +1,31 @@
 $(document).ready(function() {
-    $('.dayLong').hide();
+    $(".dayLong").hide();
+    
+    if (! window.matchMedia('screen and (min-width: 64em)').matches) {
+        $(".weekDays").hide();
+    }
 
-    $('.dayLong').click(function() {
-        $(this).toggle();
-    });
-
-    $('.dayShort').click(function() {
-        var target = $(this).attr("data-target");
-        $("#" + target).toggle();   
+    $(".dayLong, .dayShort").click(function() {
+        var target = $(this).parent().attr("id");
+        $("#" + target + " .dayShort").toggleClass("dayShortSelected");   
+        $("#" + target + " .dayLong").toggle();   
     });
 
     $(".weekToggle").click(function(e) {
-        var target = $(this).attr("data-target");
+        var week = $(this).closest(".week").attr("id");
         var display = $(this).attr("data-display") === "true";
-        $("." + target).toggle(display);
+        $("#" + week + " .weekDays").toggle(display);
+        e.preventDefault();
+    });
+
+    $(".daysToggle").click(function(e) {
+        var week = $(this).closest(".week").attr("id");
+        var display = $(this).attr("data-display") === "true";
+        if (display) {
+            $("#" + week + " .weekDays").toggle(display);
+        }
+        $("#" + week + " .dayShort").toggleClass("dayShortSelected", display);
+        $("#" + week + " .dayLong").toggle(display);
         e.preventDefault();
     });
 });
