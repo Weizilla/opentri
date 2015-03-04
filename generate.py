4bb29875fe3d69abc68bcb9b6724927d0430b61a
Wei Yang <wei@weizilla.com>
2015-03-04 15:45:24 -0600

    clean up

--- generate.py
+++ generate.py
@@ -19,6 +19,12 @@ weekHeaderTemplate = template("templates/week-header.html")
 indexTemplate = template("templates/index.html")
 
 startDate = datetime.date(2015, 1, 5)
+weekDelta = datetime.timedelta(weeks=1)
+def weekStartGen():
+    currWeek = startDate
+    while True:
+        yield currWeek
+        currWeek += weekDelta
 
 def createWeek(source):
     return Week(source)
@@ -31,53 +37,41 @@ class Generator(object):
         self.weeks = pool.map(createWeek, weeks)
 
     def generate(self, directory, filename):
-        weekGen = self.weekGen()
-
         if not os.path.isdir(directory):
             os.makedirs(directory)
-
         path = os.path.join(directory, filename)
+
+        startGen = weekStartGen()
         with open(path, "w") as output:
-            weeks = ""
-            for week in self.weeks:
-                weekStart = weekGen.next()
-                weekSubs = {}
-
-                weekHeaderSubs = {}
-                weekHeaderSubs["dayId"] = "week-{w}-h".format(w=week.num)
-                weekHeaderSubs["dayName"] = "Week {w}".format(w=week.num)
-                weekHeaderSubs["dayHeader"] = "{d.year}-{d.month}-{d.day}".format(d=weekStart)
-                weekHeaderSubs["dayLong"] = "<br/>".join(week.headers)
-
-                weekSubs["days"] = weekHeaderTemplate.substitute(weekHeaderSubs)
-
-                for day in sorted(week.days, key=lambda d: d.num):
-                    daySubs = {}
-                    daySubs["dayId"] = "week-{w}-{d}".format(w=week.num, d=day.num)
-                    daySubs["dayName"] = day.name
-                    daySubs["dayHeader"] = "<br/>".join(day.headers)
-                    daySubs["dayLong"] = "\n".join(day.workouts)
-                    weekSubs["days"] += dayTemplate.substitute(daySubs)
-
-                weekSubs["weekId"] = "week-{w}".format(w=week.num)
-                weekSubs["weekName"] = "Week {w}".format(w=week.num)
-                weekSubs["weekStart"] = "{d.year}-{d.month}-{d.day}".format(d=weekStart)
-                weekSubs["weekDate"] = "{d:%b} {d.day}".format(d=weekStart)
-                weekSubs["weekUrl"] = week.url
-                weeks += weekTemplate.substitute(weekSubs)
-
+            weeks = "".join(self.genWeek(w, startGen.next()) for w in self.weeks)
             text = indexTemplate.substitute(weeks=weeks)
             output.write(text.encode('utf8'))
             print "Wrote {p}".format(p=path)
 
-    def weekGen(self):
-        weekDelta = datetime.timedelta(weeks=1)
-        def gen():
-            currWeek = startDate
-            while True:
-                yield currWeek
-                currWeek += weekDelta
-        return gen()
+    def genWeek(self, week, start):
+        header = self.genWeekHeader(week, start)
+        days = "".join(self.genDay(d, week) for d in sorted(week.days, key=attrgetter("num")))
+        subs = {"days": header + days,
+                "weekId": "week-{w}".format(w=week.num),
+                "weekName": "Week {w}".format(w=week.num),
+                "weekStart": "{d.year}-{d.month}-{d.day}".format(d=start),
+                "weekDate": "{d:%b} {d.day}".format(d=start),
+                "weekUrl": week.url}
+        return weekTemplate.substitute(subs)
+
+    def genWeekHeader(self, week, start):
+        subs = {"dayId": "week-{w}-h".format(w=week.num),
+                "dayName": "Week {w} - {d:%b} {d.day}".format(w=week.num, d=start),
+                "dayHeader": "",
+                "dayLong": "<br/>".join(week.headers)}
+        return weekHeaderTemplate.substitute(subs)
+
+    def genDay(self, day, week):
+        subs = {"dayId": "week-{w}-{d}".format(w=week.num, d=day.num),
+                "dayName": day.name,
+                "dayHeader": "<br/>".join(day.headers),
+                "dayLong": "\n".join(day.workouts)}
+        return dayTemplate.substitute(subs)
 
 def parseArgs():
     parser = argparse.ArgumentParser(description=__doc__)
